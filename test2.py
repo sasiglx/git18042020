430ca20bbbfb73e65fcd217de5a84b7605727f3a
sasiglx <63917799+sasiglx@users.noreply.github.com>
2020-07-08 13:55:05 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 import boto3
-AWS_ACCESS_KEY_ID = 'AKIAQVXMC4NI7ELF6VX7'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = 'xxxxxxxxxxxxxxxxxxxxxx'
+AWS_SECRET_ACCESS_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 session=boto3.session(profile_name="aws_ec2_iam_user",region_name='us-east-1')
 ec2_re_ob=session.resource(service_name="ec2")
 
